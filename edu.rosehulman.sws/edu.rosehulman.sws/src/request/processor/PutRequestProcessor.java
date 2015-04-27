c73f50bcf7473d57278885ed476a1cca5f3cca22
timaeudg <timaeudg@rose-hulman.edu>
2015-04-27 05:15:53 -0400

    done with coding part

--- edu.rosehulman.sws/edu.rosehulman.sws/src/request/processor/PutRequestProcessor.java
+++ edu.rosehulman.sws/edu.rosehulman.sws/src/request/processor/PutRequestProcessor.java
@@ -9,11 +9,14 @@ import protocol.Protocol;
 import response.HttpResponseFactory;
 import response.ResponseCommand200;
 import response.ResponseCommand500;
+import server.WebServer;
 
-public class PutRequestProcessor extends PostRequestProcessor{
-
-    /* (non-Javadoc)
-     * @see request.processor.FileMayOrMayNotExist#fileExisted(protocol.HttpRequest)
+public class PutRequestProcessor extends FileMayOrMayNotExist{
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * request.processor.FileMayOrMayNotExist#fileExisted(protocol.HttpRequest)
      */
     @Override
     public HttpResponse fileExisted(HttpRequest request, File file) {
@@ -30,5 +33,34 @@ public class PutRequestProcessor extends PostRequestProcessor{
         return HttpResponseFactory.createHttpResponseFromCommand(new ResponseCommand200(), null, Protocol.CLOSE);
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * request.processor.FileMayOrMayNotExist#fileDidNotExist(protocol.HttpRequest
+     * )
+     */
+    @Override
+    public HttpResponse fileDidNotExist(HttpRequest request) {
+        // Get relative URI path from request
+        String uri = request.getUri();
+        // Get root directory path from server
+        String rootDirectory = WebServer.getServer().getRootDirectory();
+        // Combine them together to form absolute file path
+        File file = new File(rootDirectory + uri);
+
+        char[] body = request.getBody();
+
+        try {
+            FileWriter writer = new FileWriter(file, false);
+            writer.write(body);
+            writer.close();
+        } catch (Exception e) {
+            // TODO: Make 500
+            return HttpResponseFactory.createHttpResponseFromCommand(new ResponseCommand500(), null, Protocol.CLOSE);
+        }
+
+        return HttpResponseFactory.createHttpResponseFromCommand(new ResponseCommand200(), null, Protocol.CLOSE);
+    }
 
 }
